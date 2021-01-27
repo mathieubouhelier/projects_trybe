cb1775cf723a57fb0285de2bc2b5d82dc2524575
mathieu bouhelier <mathieubouhelier@gmail.com>
2021-01-27 08:41:31 -0300

    broswrouter to hashrouter

--- projects Front End/sd-04-recipes-app-8/src/index.js
+++ projects Front End/sd-04-recipes-app-8/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
@@ -9,10 +10,10 @@ import 'bootstrap/dist/css/bootstrap.css';
 import store from './store/index';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Router basename={process.env.PUBLIC_URL}>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>,
+  </Router>,
   document.getElementById('root'),
 );
